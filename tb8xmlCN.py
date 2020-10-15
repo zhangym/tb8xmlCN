6d1262927618f1c360148281c6df8f0061a13223
zhangym <ezhangym@gmail.com>
2020-10-15 09:44:44 +0800

    Add files via upload

--- tb8xmlCN.py
+++ tb8xmlCN.py
@@ -14,8 +14,8 @@ else:
     fout = open(strfout, 'w', encoding='utf-8')
     key = fin.read()
     
-    p1 = r"&amp;#(\d{5});"  # 这是我们写的正则表达式规则
-    pattern1 = re.compile(p1)  # 我们在编译这段正则表达式
+    p1 = r"&amp;#(\d{5});"  
+    pattern1 = re.compile(p1) 
     res = pattern1.findall(key)
     newKey = re.sub(p1, func, key)
     fout.write(newKey)
